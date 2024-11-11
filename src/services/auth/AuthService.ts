52e561cb81672812d3ac02d8c983cb86739b31fe
william <santifon15@gmail.com>
2024-11-11 09:21:13 -0500

    Add basic auth service impl

---
+++ src/services/auth/AuthService.ts
@@ -0,0 +1,33 @@
+import { $ } from '@builder.io/qwik';
+import { AuthRepository } from '~/infrastructure/persistence/repositories/auth.repository';
+import { LoginRequest, LoginResponse } from '~/models/auth.types';
+import { BaseResponse } from '~/models/common/baseResponse.type';
+
+const authRepository = new AuthRepository();
+
+export const isAuthenticated = $(async (user_id: string) => {
+  const token = await authRepository.getJWT(user_id);
+  return token !== null;
+});
+
+export const login = $((req: LoginRequest): BaseResponse<LoginResponse> => {
+  if(req.username !== "admin" || req.password !== "admin"){
+    return {
+      success: false,
+      code: 401,
+      errorMessage: "Invalid username or password"
+    }
+  }
+
+  let response: BaseResponse<LoginResponse> = {
+    success: true,
+    code: 200
+  }
+  let loginResponse: LoginResponse = {
+    username: req.username,
+    token: "token"
+  };
+  authRepository.saveJWT(req.username, loginResponse.token);
+  response.value = loginResponse;
+  return response;
+});
