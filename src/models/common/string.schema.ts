73f54ac159f2592fb7689551912e0efd9b29424f
william <santifon15@gmail.com>
2025-02-26 18:48:03 -0500

    clean repo

--- src/models/common/string.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "@builder.io/qwik-city";
-
-export const NonEmptyOrWhitSpaceStringModel = z
-  .string()
-  .min(1, "Value can not be empty")
-  .trim();
--- src/models/common/string.type.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "@builder.io/qwik-city";
-import { NonEmptyOrWhitSpaceStringModel } from "./string.schema";
-
-export type NonEmptyOrWhitSpaceString = z.infer<
-  typeof NonEmptyOrWhitSpaceStringModel
->;
