fe0d973160c0bd6f2429a5dfc5867cede10da5f3
william <santifon15@gmail.com>
2025-02-26 18:48:03 -0500

    clean repo

--- src/models/auth.types.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "@builder.io/qwik-city";
-import { LoginRequestModel, LoginResponseModel } from "./auth.schema";
-
-export type LoginRequest = z.infer<typeof LoginRequestModel>;
-export type LoginResponse = z.infer<typeof LoginResponseModel>;
--- src/models/common/baseResponse.type.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "@builder.io/qwik-city";
-import { BaseResponseModel } from "./baseResponse.schema";
-
-//export type BaseResponse<T> = z.infer<typeof BaseResponseModel<z.ZodType<T>>>;
-export type BaseResponse<T> = z.infer<typeof BaseResponseModel>;
