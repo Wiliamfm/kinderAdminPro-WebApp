b00a3541d5e4a25a44602284c9e4fc416880548a
william <santifon15@gmail.com>
2025-03-06 06:10:32 -0500

    Add logout option

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,7 +31,7 @@ export default component$(() => {
                 <a href="/" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Configuración</a>
               </li>
               <li>
-                <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
+                <a href="/auth/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
               </li>
             </ul>
           </div>
