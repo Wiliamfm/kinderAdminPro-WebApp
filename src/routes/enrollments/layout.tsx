6fc8ce697fd4a94ba0b54ac667ef4d5a28035e43
william <santifon15@gmail.com>
2025-04-22 23:20:16 -0500

    Create middleware for enrollments routes

---
+++ src/routes/enrollments/layout.tsx
@@ -0,0 +1,10 @@
+import { RequestHandler } from "@builder.io/qwik-city";
+import { getUserStatus } from "~/services/identity.service";
+import { IdentityRolesEnum } from "~/types/identity.types";
+
+export const onRequest: RequestHandler = async (event) => {
+  const user = await getUserStatus();
+  if (!user || user.role !== IdentityRolesEnum.Admin) {
+    throw event.redirect(308, "/");
+  }
+};
