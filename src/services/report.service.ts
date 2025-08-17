0fed22791b1ab59388c77df832006a55fd498983
william <santifon15@gmail.com>
2025-08-17 13:42:41 -0500

    Create view to update bulletins

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -27,3 +27,44 @@ export const deleteBulletin = server$(async function(id: number) {
 
   return true;
 });
+
+export const getBulletin = server$(async function(id: number) {
+  const { data, error } = await getSupabase().from("bulletins").select("*").eq("id", id).single();
+  if (error) {
+    console.error("Unable to get bulletin: ", error);
+    return null;
+  }
+
+  return data as Bulletin;
+});
+
+export const createBulletin = server$(async function(name: string, type: string) {
+  const { data, error } = await getSupabase().from("bulletins").insert({
+    name: name,
+    type: type,
+  }).select().single();
+  if (error) {
+    console.error("Unable to create bulletin.");
+    return null;
+  }
+
+  return data as Bulletin;
+});
+
+export const updateBulletin = server$(async function(id: number, name: string, type: string) {
+  const bulletin = await getBulletin(id);
+  if (!bulletin) {
+    return null;
+  }
+
+  const { data, error } = await getSupabase().from("bulletins").update({
+    name: name,
+    type: type,
+  }).eq("id", id).select().single();
+  if (error) {
+    console.error("Unable to update bulletin: ", error);
+    return null;
+  }
+
+  return data as Bulletin;
+});
