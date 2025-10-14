61d031b48ea2b72de767bde76f4bfe9758d490b9
william <santifon15@gmail.com>
2025-10-14 05:16:57 -0500

    Remove unused menus

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -58,22 +58,6 @@ export default component$<HeaderProps>(({ user }) => {
             <ul class="py-2" aria-labelledby="user-menu-button">
               <li>
                 <a
-                  href="/"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Configuración
-                </a>
-              </li>
-              <li>
-                <a
                   href="/auth/logout"
                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                 >
