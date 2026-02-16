b8fafa5e9b746434c0853b3d28e25f0bf570c03e
william <santifon15@gmail.com>
2026-02-16 03:45:30 -0500

    Delete project

--- src/routes/index.tsx
+++
@@ -1,5 +0,0 @@
-import ProtectedPage from "~/components/ProtectedPage";
-
-export default function Home() {
-  return <ProtectedPage title="Inicio" heading="Panel principal" description="Bienvenido a KinderAdminPro." />;
-}
