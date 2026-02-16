cc710771269edb9f9d1346d6ab76f88027327d96
william <santifon15@gmail.com>
2026-02-16 02:11:07 -0500

    Delete all

--- src/loaders/payroll.loader.ts
+++
@@ -1,7 +0,0 @@
-import { routeLoader$ } from "@builder.io/qwik-city";
-import { getEmployeesJobs } from "~/services/payroll.service";
-
-export const useGetEmployeeJobs = routeLoader$(async () => {
-  const employeeJobs = await getEmployeesJobs();
-  return { employeeJobs };
-});
