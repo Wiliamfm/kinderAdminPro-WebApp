0ad0dee9616ece40e0664f95f21440c10bff4928
william <santifon15@gmail.com>
2026-02-23 18:45:08 -0500

    update vide allowed host config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [devtools(), solidPlugin()],
   server: {
     port: 3000,
+    allowedHosts: ['kinderadminpro-webapp-2.onrender.com']
   },
   build: {
     target: 'esnext',
